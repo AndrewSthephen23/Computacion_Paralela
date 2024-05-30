7d7edc0e974103f0f7d2fce4323e9edc83752d55
AndrewSthephen23 <andrei3elcausita@gmail.com>
2024-05-30 17:32:17 -0500

    ClientPython comentado

--- PC3/ClientPython/Client.py
+++ PC3/ClientPython/Client.py
@@ -1,51 +1,72 @@
-import random
-import time
-import pika
-import uuid
-import flet as ft
+# Importamos las librerias
+import random 
+import time #Para manejar el tiempo y pausas
+import pika #Para mejorar la comunicación con RabbitMQ
+import uuid #Para generar un ID unico
+import flet as ft #Para crear la interfaz grafica
 
+#Define la clase System que maneja la logica principal del sistema
 class System:
+    # Variables para el usuario, el ruc y el carrito de compras
     username = ""
     ruc = ""
     cart = {}
 
+    # Constructor de la clase
     def __init__(self):
+        # Credenciales para conectarse a RabbitMQ
         credentials = pika.PlainCredentials('chan', 'chan')
+        # Establece la coneccion con RabbitMQ
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters('localhost', 5672, 'venta_host', credentials))
-
+        
+        # Crea un canal para la comunicacion 
         self.channel = self.connection.channel()
 
+        # Declara una cola exclusiva para las respuestas
         result = self.channel.queue_declare(queue='', exclusive=True)
         self.callback_queue = result.method.queue
 
+        # Configura el consumidor para recibir mensajes de la cola de respuestas
         self.channel.basic_consume(
             queue=self.callback_queue,
             on_message_callback=self.on_response,
             auto_ack=True)
 
+        # Inicializa las variables de respuesta y correlación
         self.response = None
         self.corr_id = None
 
+    # Metodo para editar el carrito de compras
     def edit_cart(self, id, type, tf):
+        # Si el ID del producto ya esta en el carrito, actualiza la cantidad
         if id in self.cart:
             self.cart[id] += type
+            # Si la cantidad es 0, elimina el ID del producto del carrito
             if self.cart[id] == 0:
                 del self.cart[id]
         else:
+            # Si el ID del producto no esta en el carrito, lo agrega con cantidad 1
             self.cart[id] = 1
+        # Actualiza el valor de la caja de texto con la cantidad actual del producto    
         tf.value = str(self.cart[id]) if id in self.cart else "0"
         tf.update()
     
+    # Metodo para limpiar el carrito de compras
     def clear_cart(self,tf_list):
+        # Limpia el carrito de compras
         self.cart = {}
+        # Actualiza todos los campos de texto a "0"        
         for tf in tf_list:
             tf.value = "0"
             tf.update()
 
+    # Metodo para obtener la lista de productos desde el servidor
     def get_products(self):
         self.response = None
         self.corr_id = str(uuid.uuid4())
+
+        # Publica una solicitud de productos en la cola de peticiones
         self.channel.basic_publish(
             exchange='',
             routing_key='go-python-queue',
@@ -54,16 +75,23 @@ class System:
                 correlation_id=self.corr_id,
             ),
             body="get_products")
+        
+        # Espera la respuesta del servidor
         self.connection.process_data_events(time_limit=None)
+        # Devuelve la respuesta recibida
         return str(self.response, "utf-8")
 
+    # Callback para manejar la respuesta del servidor
     def on_response(self, ch, method, props, body):
+        # Si el ID de la respuesta coincide con el ID de la petición, guarda la respuesta
         if self.corr_id == props.correlation_id:
             self.response = body
 
+    # Metodo para generar una factura
     def generate_bill(self, mensaje):
         self.response = None
         self.corr_id = str(uuid.uuid4())
+        # Publica la solicitud de generacion de factura en la cola
         self.channel.basic_publish(
             exchange='',
             routing_key='go-python-queue',
@@ -72,22 +100,29 @@ class System:
                 correlation_id=self.corr_id,
             ),
             body=mensaje)
+        # Espera la respuesta del servidor
         self.connection.process_data_events(time_limit=None)
+        # Devuelve la respuesta recibida
         return str(self.response, "utf-8")
 
+# Crea un objeto de la clase System
 system = System()
 
+# Define la funcion para la pagina inicial de la aplicacion
 def initial_page(page: ft.Page):
+    # Limpia la pagina y reinicia las variables del sistema
     page.clean()
     system.username = ""
     system.ruc = ""
     system.cart = {}
 
+    # Funcion para manejar el inicio de session
     def login(username, ruc):
         system.username = username
         system.ruc = ruc
         main_page(page)
 
+    # Define los campos de texto para el nombre y el RUC
     username = ft.TextField(
         value="",
         border_color="white",
@@ -98,6 +133,8 @@ def initial_page(page: ft.Page):
         border_color="white",
         width=200
     )
+
+    # Agrega los elementos a la pagina
     page.add(ft.SafeArea(
         content=ft.Container(
             content=ft.Column(
@@ -137,36 +174,39 @@ def initial_page(page: ft.Page):
         minimum=30
     ))
 
+# Define la funcion para mostrar los productos
 def show_products(page: ft.Page):
-    prods = system.get_products()
-    prods = prods[:-1]
-    prods = prods.split(";")
-    page.clean()
-    rows_list = []
-    tf_list = []
+    prods = system.get_products() # Obtiene los productos del servidor
+    prods = prods[:-1] # Elimina el ";" al final
+    prods = prods.split(";") # Separa la cadena en una lista de productos
+    page.clean() # Limpia la pagina 
+    rows_list = [] # Lista para almacenar las filas de la tabla
+    tf_list = [] # Lista para almacenar los campos de texto
     for prod in prods:
-        items = prod.split(",")
-        id = int(items[0])
+        items = prod.split(",") # Separa los detalles del producto
+        id = int(items[0]) # Obtiene el ID del producto
         tf = ft.TextField(
-            value=str(system.cart[id]) if id in system.cart else "0",
+            value=str(system.cart[id]) if id in system.cart else "0", # Muestra la cantidad en el carrito
             width=50,
             height=30,
             text_align=ft.TextAlign.CENTER,
             disabled=True,
         )
-        tf_list.append(tf)
+        tf_list.append(tf) # Añade el campo de texto a la lista
+
+        # Funciones para añadir o quitar productos del carrito
         def create_remove_func(id=id, tf=tf):
             return lambda e, item_id=id: system.edit_cart(item_id, -1, tf)
-
         def create_add_func(id=id, tf=tf):
             return lambda e, item_id=id: system.edit_cart(item_id, 1, tf)
 
+        # Crea las filas de la tabla
         temp = [
             ft.DataCell(
                 content=ft.Text(items[0])),
             ft.DataCell(
                 ft.Text(items[1])),
-            ft.DataCell(
+            ft.DataCell(    
                 ft.Text(items[2])),
             ft.DataCell(
                 ft.Row(
@@ -176,7 +216,7 @@ def show_products(page: ft.Page):
                             icon_color="blue400",
                             icon_size=20,
                             tooltip="Remover del carrito",
-                            on_click=create_remove_func()
+                            on_click=create_remove_func() # Llama a la función para remover del carrito
                         ),
                         tf,
                         ft.IconButton(
@@ -184,7 +224,7 @@ def show_products(page: ft.Page):
                             icon_color="blue400",
                             icon_size=20,
                             tooltip="Agregar al carrito",
-                            on_click=create_add_func()
+                            on_click=create_add_func() # Llama a la funcion para añadir al carrito
                         ),
                     ],
                     alignment=ft.MainAxisAlignment.CENTER,
@@ -192,8 +232,9 @@ def show_products(page: ft.Page):
                 )
             )
         ]
-        rows_list.append(temp)
+        rows_list.append(temp) # Añade la fila a la lista
 
+    # Añade los elementos a la pagina
     page.add(ft.SafeArea(
         content=ft.Container(
             content=ft.Column(
@@ -230,7 +271,7 @@ def show_products(page: ft.Page):
                         text="Generar Factura",
                         width=200,
                         height=40,
-                        on_click=lambda e: generate_bill(page)
+                        on_click=lambda e: generate_bill(page) # Llama a la funcion para generar la factura
                     ),
                     ft.Row(
                         controls=[
@@ -238,13 +279,13 @@ def show_products(page: ft.Page):
                                 text="Borrar Carrito",
                                 width=200,
                                 height=40,
-                                on_click=lambda e: system.clear_cart(tf_list)
+                                on_click=lambda e: system.clear_cart(tf_list) # Llama a la funcion para borrar el carrito
                             ),
                             ft.ElevatedButton(
                                 text="Volver",
                                 width=200,
                                 height=40,
-                                on_click=lambda e: main_page(page),
+                                on_click=lambda e: main_page(page),# Llama a la funcion para volver a la pagina principal
                             )
                         ],
                         alignment=ft.MainAxisAlignment.CENTER,
@@ -257,18 +298,21 @@ def show_products(page: ft.Page):
             ),
         )))
 
+# Funcion para generar la factura
 def generate_bill(page: ft.Page):
-    if len(system.cart) == 0:
+    if len(system.cart) == 0:# Si el carrito esta vacio, no hace nada
         return
+    
+    # Funcion para enviar la factura
     def send_bill(e):
         mensaje = system.username + ";" + system.ruc + ";"
-        for id in system.cart:
+        for id in system.cart:# Añade los productos del carrito  al mensaje
             mensaje += str(id) + "," + str(system.cart[id]) + "/"
-        mensaje = mensaje[:-1]
-        system.cart = {}
-        status = system.generate_bill(mensaje)
-        main_page(page,status)
-    page.clean()
+        mensaje = mensaje[:-1] # Elimina el ultimo caracter del mensaje
+        system.cart = {} # Vacia el carrito
+        status = system.generate_bill(mensaje) # Genera la factura
+        main_page(page,status) # Navega a la pagina principal con el status de la factura
+    page.clean() # Limpia la pagina
     page.add(ft.SafeArea(
         content=ft.Container(
             content=ft.Column(
@@ -299,13 +343,13 @@ def generate_bill(page: ft.Page):
                         text="Generar Factura",
                         width=200,
                         height=40,
-                        on_click=send_bill
+                        on_click=send_bill # Llama a la funcion para enviar la factura
                     ),
                     ft.ElevatedButton(
                         text="Volver",
                         width=200,
                         height=40,
-                        on_click=lambda e: main_page(page),
+                        on_click=lambda e: main_page(page), # Llama a la funcion para volver a la pagina principal
                     )
                 ],
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
@@ -317,8 +361,9 @@ def generate_bill(page: ft.Page):
         minimum=30
     ))
 
+# Funcion para mostrar la pagina principal
 def main_page(page: ft.Page, message=""):
-    page.clean()
+    page.clean() # Limpia la pagina
     user_text = ft.Text(
         value="Bienvenido " + system.username + " con RUC: " + system.ruc,
         font_family="Arial",
@@ -341,19 +386,19 @@ def main_page(page: ft.Page, message=""):
                         text="Generar Factura",
                         width=200,
                         height=40,
-                        on_click=lambda e: generate_bill(page)
+                        on_click=lambda e: generate_bill(page) # Llama a la funcion para generar la factura
                     ),
                     ft.ElevatedButton(
                         text="Ver Productos",
                         width=200,
                         height=40,
-                        on_click=lambda e: show_products(page)
+                        on_click=lambda e: show_products(page) # Llama a la funcion para mostrar los productos
                     ),
                     ft.ElevatedButton(
                         text="Salir",
                         width=200,
                         height=40,
-                        on_click=lambda e: initial_page(page)
+                        on_click=lambda e: initial_page(page) # Llama a la funcion para cerrar la sesion
                     )
                 ],
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
@@ -364,19 +409,20 @@ def main_page(page: ft.Page, message=""):
         minimum=30
     ))
 
+# Funcion principal para configurar la ventana
 def main_window(page: ft.Page):
-    page.window_height = 750
-    page.window_width = 1000
-    page.window_resizable = False
-    page.window_full_screen = False
-    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
-    page.title = "Sistema Venta"
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.window_center()
-    page.window_visible = True
-    initial_page(page)
+    page.window_height = 750 # Establece la altura de la ventana
+    page.window_width = 1000 # Establece el ancho de la ventana
+    page.window_resizable = False # La ventana no es redimensionable
+    page.window_full_screen = False # La ventana no es de pantalla completa
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER # Alineacion horizontal centrada
+    page.title = "Sistema Venta" # Titulo de la ventana
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER # Alineacion horizontal centrada
+    page.window_center() # Centra la ventana
+    page.window_visible = True # Hace visible la ventana
+    initial_page(page) # Muestra la pagina inicial
 
-
+# Inicia la aplicacion
 ft.app(target=main_window, view=ft.AppView.FLET_APP_HIDDEN)
 
 
